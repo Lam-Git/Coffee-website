eb097484459d4fba5eb62d7404cb23b39075b320
Lam Nguyen <lsnguyen0@gmail.com>
2021-03-09 17:56:30 -0800

    commit

---
+++ static/main.js
@@ -0,0 +1,21 @@
+const colors = ["blue", "pink", "green", "purple", "red"];
+
+let i = 0;
+
+Array.from(document.querySelectorAll(".nav-link")).forEach((item) => {
+  item.style.cssText = `background-color: ${colors[i++]}`;
+});
+
+Array.from(document.querySelectorAll(".navigation-button")).forEach((item) => {
+  item.onclick = () => {
+    item.parentElement.parentElement.classList.toggle("change");
+  };
+});
+/this bottom code is to make the bar transaction smooth/;
+const container = document.querySelector(".container");
+document.querySelector(".open-navbar-icon").addEventListener("click", () => {
+  container.classList.add("change");
+});
+document.querySelector(".close-navbar-icon").addEventListener("click", () => {
+  container.classList.remove("change");
+});
